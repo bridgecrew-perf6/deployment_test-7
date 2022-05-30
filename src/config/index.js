d1a48ea03207cbc3a8a8f6534f16597e19340eba
niansoon <simniansoon@hotmail.com>
2022-05-30 12:09:07 +0800

    something

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const env = process.env.REACT_APP_ENV;
 //Environment Variables are variables that is used by the application itself
 //Details such as secret keys/environment are part of the environment varialbes
 //In react, environment variables start with REACT_APP_
-
+console.log(env);
 let config = {
     baseURL: "https://localhost"
 }
