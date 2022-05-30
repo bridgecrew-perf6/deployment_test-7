9a7f62b4fc9669a5e44f3d5a217a92404eb9fc6e
niansoon <simniansoon@hotmail.com>
2022-05-30 11:35:18 +0800

    add environment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import config from './config/index';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <div>BaseURL: {config.baseURL}</div>
       </header>
     </div>
   );
